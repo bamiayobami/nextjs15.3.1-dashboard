d4514ef8f46b677360f94a093af6df214b0764db
bamiayobami <132226030+bamiayobami@users.noreply.github.com>
2025-04-21 07:51:19 +0100

    Approved bcript as built dependency
    - run pnpm approve-builds
    
    - choosed bcrypt as the only built dependency
    
    - just to satisfy vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // transpilePackages: ['@heroicons/react', 'bcrypt'], // added it myself to help deploy to vercel - thanks to gemini
 };
 
 export default nextConfig;
