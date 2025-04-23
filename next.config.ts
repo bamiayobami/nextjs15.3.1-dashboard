ef19a0df2732ae69d6a9e28f7bb3232677944968
bamiayobami <132226030+bamiayobami@users.noreply.github.com>
2025-04-23 21:29:40 +0100

    PPR
    - testing Partial Prerendering
    -  this will be deleted immediately after testing
    - this is simply experimental today
    - PPR will be the default of rendering in Nextjs apps and similar frameworks in its stable future

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: 'incremental'
+  }
   
   // transpilePackages: ['@heroicons/react', 'bcrypt'], // added it myself to help deploy to vercel - thanks to gemini
 };
