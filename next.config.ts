5a15c93d18cd5fef9d15a85ce039191e2af0bdd8
bamiayobami <132226030+bamiayobami@users.noreply.github.com>
2025-04-24 10:08:58 +0100

    Revert "PPR"
    This reverts commit ef19a0df2732ae69d6a9e28f7bb3232677944968.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental'
-  }
   
   // transpilePackages: ['@heroicons/react', 'bcrypt'], // added it myself to help deploy to vercel - thanks to gemini
 };
