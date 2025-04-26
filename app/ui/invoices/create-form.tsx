30398b1cda90e96cdc15727d8a53f9af5d34a045
bamiayobami <132226030+bamiayobami@users.noreply.github.com>
2025-04-26 16:59:32 +0100

    Updating and Deleting Invoice
    - create dynamic route segment with invoice [id]s
    - using Server Actions to fetch data and prepopulate the update form
    - used 'zod' to validate the form inputs
    - updated the database and revalidating the Next.js cache with Next.js APIs
    - implemented the delete button functionality

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,7 +7,6 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { create } from 'domain';
 import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
